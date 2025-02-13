68aeab8e84bd453bf0349a055ee0057454ebf682
Saud Khan <143578711+saud9422@users.noreply.github.com>
2025-02-13 12:26:38 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,6 +4,9 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		StatementHW obj1=new StatementHW();
 		obj1.insertData();
+		obj1.deleteDate();
+		obj1.updateData();
+		obj1.showData();
 	}
 
 }
